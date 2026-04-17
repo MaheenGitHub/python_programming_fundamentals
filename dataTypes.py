295cf3827a22b55cd0da6f120049bb68ddec07d7
Maheen Fatima <maheen19pgc@gmail.com>
2026-04-17 16:04:29 +0500

    int, float, string , bool data type

---
+++ dataTypes.py
@@ -0,0 +1,39 @@
+#python -> object orinted so....
+#data types -> classes
+#variabes -> objects/instances of these classes
+
+var_1 = 3
+print (type(var_1)) # <class 'int'> 
+print (type("hello"))
+
+#int +ve to -ve infinity (constraint system memory)
+# prefixes
+    #binary 0b , 0B 
+    #octal  0o , 0O
+    #hexadecimal 0x , 0X
+
+print(0b10) #2 -> binary of 10 is 2
+
+#float 4.2
+#strings , sequence of characters 
+#boolean
+
+print(10 + 2.5)
+#printg -> name error and not syntax error
+
+name = "maheen"
+print(name[1]) #string as array
+print(2 * name )
+
+#print(name + var_1) #TypeError
+print("Maheen\'s \"new\" work") #backslash as escape letters
+print("use \\n double slash")
+
+#oool variable 
+#var = true
+#print(var) name error
+
+a=1
+b=2
+var = a<b
+print(var)
